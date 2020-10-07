c6266d86da8dd691928892d123c900170f941ce4
Margrete Rolighed Xie <36185407+Margretexie@users.noreply.github.com>
2020-10-07 15:04:24 +0200

    Update sketch.js

--- InternetBackroads/sketch.js
+++ InternetBackroads/sketch.js
@@ -1,49 +1,46 @@
 console.log("Extension running");
 
 var url = ["http://savbladet.dk/Index.htm", "https://neave.tv/", "http://www.windows93.net/", "https://www.spacejam.com/",
-          "http://manhole-covers.net/galerie/slide.php3?f=0", "https://www.nobodyhere.com/justme/me.here",
+           "https://www.nobodyhere.com/justme/me.here",
           "https://www.biodiversitet.nu/", "http://encyclopedia.uia.org/en", "https://youtu.be/AHrCI9eSJGQ",
-          "https://skins.webamp.org/", "https://timberfestival.org.uk/soundsoftheforest-soundmap/", "https://pointerpointer.com/", "http://make-everything-ok.com/", "https://graphics.reuters.com/ENVIRONMENT-PLASTIC/0100B275155/index.html?",];
+          "https://skins.webamp.org/", "https://timberfestival.org.uk/soundsoftheforest-soundmap/", "http://make-everything-ok.com/",
+          "https://graphics.reuters.com/ENVIRONMENT-PLASTIC/0100B275155/index.html?","https://ikeamuseum.com/sv/ikea-kataloger/",
+          "https://imisstheoffice.eu/", "http://www.fallingfalling.com/", "http://pixelsfighting.com/", "https://www.harald-nyborg.dk/",
+          "http://grishaverse.com/", "https://www.theinspiration.com/2020/08/pencil-artworks-by-bashir-sultani/", "https://archiveofourown.org/",
+          "https://www.funkbrewing.com/elizabethtown/", "https://blacklivesmatters.carrd.co/", "http://isitfridayyet.net/", "https://www.dndbeyond.com/",
+          "https://aartdevos.dk/", "https://www.foxnews.com/"," https://www.msnbc.com/", "https://politiken.dk/", "https://itch.io/",
+          "https://driveandlisten.herokuapp.com/", "https://musescore.com/", "http://astronaut.io/", "https://hex.frvr.com/",
+          "https://www.photopea.com/", "https://www.flickeringmyth.com/", "http://www.welcometonightvale.com/",
+          "https://www.lexilogos.com/english/index.htm", "https://www.oldbookillustrations.com/",
+          "https://www.nerdfitness.com/blog/a-beginners-guide-to-handstands/", "https://da.wikipedia.org/wiki/Ymerdrys"];
 var link = url[Math.floor(Math.random() * url.length)];
 var body = document.body,
     html = document.documentElement;
-var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+// var height = window.innerHeight;
+var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 var width = window.innerWidth;
-// var arrow_position = [Math.random() * width, Math.random() * height];
-var arrow_position = [100, 100];
+var arrow_x = Math.random() * width;
+var arrow_y = Math.random() * height;
 
 //Code for displaying <extensionDir>/images/myimage.png:
   //var imgURL = chrome.runtime.getURL("images/myimage.png");
   //document.getElementById("someImage").src = imgURL;
 
-var arrowdest = "images/arrowbutton.png";
-var arrow = document.createElement("img");
-arrow.src = chrome.extension.getURL(arrowdest);
-arrow.position = arrow_position;
 
+  var arrow = new Image();
+  arrow.src = chrome.runtime.getURL("images/arrowbutton.png");
+  arrow.style.position = "absolute";
+  arrow.style.left = (arrow_x - 50).toString() + "px";
+  arrow.style.top = (arrow_y - 50).toString() + "px";
+  document.body.appendChild(arrow);
 
-console.log(url);
-// location.replace(link);
+console.log(arrow_x, arrow_y);
 
 document.addEventListener("click", check_position);
 
 function check_position(event) {
-  if (Math.sqrt(Math.pow((event.clientX - arrow_position[0]), 2) + Math.pow((event.clientY - arrow_position[1]), 2)) < 55) {
+  console.log(event.pageX, event.pageY);
+  if (Math.sqrt(Math.pow((event.pageX - arrow_x), 2) + Math.pow((event.pageY - arrow_y), 2)) < 55) {
   window.location.href = link;
   }
 }
-
-// //1. create button
-// var button = document.createElement("button");
-// button.innerHTML = "Do Something";
-//
-// // 2. Append somewhere
-// var body = document.getElementsByTagName("body")[0];
-// body.appendChild(button);
-//
-// // 3. Add event handler
-// button.addEventListener ("click", function() {
-//   window.location.href = "http://savbladet.dk/Index.htm";
-// });
-
-//console.log(button.function);
